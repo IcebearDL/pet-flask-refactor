3dab1b6c1298adec5a825e546f959a3f3cccaef0
IcebearDL <u201616984@hust.edu.cn>
2020-01-25 20:09:19 +0800

    Add empty projectList page.

---
+++ src/pages/ProjectList/model.js
@@ -0,0 +1,21 @@
+import { message } from "antd"
+
+const Model = {
+
+    namespace: "ProjectList",
+
+    state: {
+
+    },
+
+    reducers: {
+
+    },
+
+    effects: {
+
+    }
+
+}
+
+export default Model
