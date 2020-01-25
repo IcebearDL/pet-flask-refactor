824ec33a24c53e0bd04920abd4f869a615bf3005
IcebearDL <u201616984@hust.edu.cn>
2020-01-25 20:10:13 +0800

    Add login page api.

---
+++ src/services/login.js
@@ -0,0 +1,12 @@
+import request from '../utils/request'
+
+//登陆接口
+export async function Login(body) {
+    return request('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+}
