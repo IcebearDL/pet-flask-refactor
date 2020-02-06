9557464a253dccfa229063356babafaa0bec3c79
IcebearDL <u201616984@hust.edu.cn>
2020-02-06 20:49:45 +0800

    fix: Fix the userless import.

--- src/pages/CRFdetail/components/CycleRecord.js
+++ src/pages/CRFdetail/components/CycleRecord.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import { connect } from 'dva'
-import { PageHeader, Layout } from "antd"
-import styles from '../style.css'
+// import { PageHeader, Layout } from "antd"
+// import styles from '../style.css'
 
 class CycleRecord extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-    render() {
-        return (
-            <div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
 }
 
 function mapStateToProps(state) {
-    return {}
+  return {}
 }
 
 export default connect(mapStateToProps)(CycleRecord)
