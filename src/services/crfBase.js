3296ca8b07fcc55d5815bf7bcfe39323e7abf802
IcebearDL <u201616984@hust.edu.cn>
2020-02-18 15:53:57 +0800

    feat(crfBase.js): Add apis.

--- src/services/crfBase.js
+++ src/services/crfBase.js
@@ -27,3 +27,53 @@ export async function DeleteCycle({ sample_id }) {
     method: 'DELETE'
   })
 }
+
+//获取生存期随访信息
+export async function FetchInterviewTable({ sample_id }) {
+  return request(`/interview_table/${sample_id}`, {
+    method: 'GET'
+  })
+}
+
+//添加或修改生存期随访信息
+export function ModifyInterview({ sample_id, body }) {
+  return request(`/interview/${sample_id}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+}
+
+//删除生存期随访
+export function DeleteInterview({ sample_id, interview_id }) {
+  return request(`/interview/${sample_id}/${interview_id}`, {
+    method: 'DELETE'
+  })
+}
+
+//获取项目总结
+export async function FetchSummary({ sample_id }) {
+  return request(`/summary/${sample_id}`, {
+    method: 'GET'
+  })
+}
+
+//提交项目总结
+export async function ModifySummary({ sample_id, body }) {
+  return request(`/summary/${sample_id}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+}
+
+//获取全部严重不良事件
+export function FetchAdverseEventAll({ sample_id }) {
+  return request(`/adverse_event_table_all/${sample_id}`, {
+    method: 'GET'
+  })
+}
