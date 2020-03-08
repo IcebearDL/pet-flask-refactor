446b39f300940c82ca36e24fb5502a3661ef8824
IcebearDL <u201616984@hust.edu.cn>
2020-03-08 20:09:01 +0800

    feat(.umirc.js): Add targets and babel-plugin-transform-react-remove-prop-types, fix code format pro

--- .umirc.js
+++ .umirc.js
@@ -1,29 +1,28 @@
-
 // ref: https://umijs.org/config/
+
 export default {
   treeShaking: true,
   history: 'hash',
   publicPath: '/',
   theme: {
-    "primary-color": "#4279E4",
+    'primary-color': '#4279E4',
     // "primary-color": "#1DA57A",
-    "heading-color": "#191919",
-    "text-color": "#404040",
-    "text-color-secondary": "#666666"
+    'heading-color': '#191919',
+    'text-color': '#404040',
+    'text-color-secondary': '#666666'
   },
   //webpack copy favicon 文件，由于document.ejs没有引入，所以不会自动打包输出
-  copy: [
-    '/src/assets/favicon.png'
-  ],
+  copy: ['/src/assets/favicon.png'],
   proxy: {
-    "/api": {
-      target: "http://39.96.191.139",
+    '/api': {
+      target: 'http://39.96.191.139',
       changeOrigin: true,
       pathRewrite: {
-        "^/api": ""
+        '^/api': ''
       }
     }
   },
+  targets: { chrome: 49, firefox: 45, safari: 10, edge: 13, ios: 10 },
   routes: [
     {
       path: '/',
@@ -39,7 +38,7 @@ export default {
       routes: [
         {
           path: '/project',
-          component: '../pages/ProjectList/index',
+          component: '../pages/ProjectList/index'
         },
         {
           path: '/project/:id/sample',
@@ -54,22 +53,32 @@ export default {
   ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: false,
-      title: 'pet-flask-refactor',
-      dll: false,
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: false,
+        title: 'pet-flask-refactor',
+        dll: false,
 
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
-      },
-    }],
-  ],
+        routes: {
+          exclude: [
+            /models\//,
+            /services\//,
+            /model\.(t|j)sx?$/,
+            /service\.(t|j)sx?$/,
+            /components\//
+          ]
+        }
+      }
+    ],
+    [
+      'babel-plugin-transform-react-remove-prop-types',
+      {
+        mode: 'wrap',
+        ignoreFilenames: ['node_modules']
+      }
+    ]
+  ]
 }
