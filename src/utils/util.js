a659bf3df826ab07b161a550dd5b93d7d9594b4e
IcebearDL <u201616984@hust.edu.cn>
2020-03-30 21:28:42 +0800

    improvement(util.js): Add removeNull for request.

--- src/utils/util.js
+++ src/utils/util.js
@@ -8,3 +8,26 @@ export function checkLogin() {
   }
   return true
 }
+
+export function removeNull(data) {
+  if (Object.prototype.toString.call(data) !== '[object Object]') {
+    throw new Error('request data is not a object.')
+  }
+
+  // 两层去掉null
+  for (let key in data) {
+    if (data[key] === null || JSON.stringify(data[key]) === '{}') {
+      delete data[key]
+    } else if (
+      Object.prototype.toString.call(data[key]) === '[object Object]'
+    ) {
+      for (let _key in data[key]) {
+        if (data[key][_key] === null) {
+          delete data[key][_key]
+        }
+      }
+    }
+  }
+
+  return data
+}
