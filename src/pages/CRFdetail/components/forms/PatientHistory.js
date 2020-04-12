7fa3ad62dd931bc2b2bc4e395a25531df140482b
IcebearDL <u201616984@hust.edu.cn>
2020-04-12 14:01:16 +0800

    fix: Delete userless submit code.

--- src/pages/CRFdetail/components/forms/PatientHistory.js
+++ src/pages/CRFdetail/components/forms/PatientHistory.js
@@ -1,17 +1,7 @@
 import React from 'react'
 import { connect } from 'dva'
 import PropTypes from 'prop-types'
-import {
-  Col,
-  Form,
-  DatePicker,
-  Button,
-  Radio,
-  Input,
-  Checkbox,
-  Switch,
-  Select
-} from 'antd'
+import { Col, Form, DatePicker, Button, Radio, Input, Checkbox, Switch, Select } from 'antd'
 import moment from 'moment'
 import { getSampleId } from '@/utils/location'
 import styles from '../../style.css'
@@ -46,12 +36,7 @@ class PatientHistory extends React.Component {
   }
 
   handleStateChange = (type, e) => {
-    if (
-      type === 'drug_use' ||
-      type === 'drug_allergy' ||
-      type === 'tumor_ill' ||
-      type === 'surgery'
-    ) {
+    if (type === 'drug_use' || type === 'drug_allergy' || type === 'tumor_ill' || type === 'surgery') {
       this.setState({ [type]: e.target.value })
     } else {
       this.setState({ [type]: e })
@@ -96,9 +81,7 @@ class PatientHistory extends React.Component {
         }
         // 过滤日期传值
         if (values.drinking_quit_time) {
-          values.drinking_quit_time = values.drinking_quit_time.format(
-            'YYYY-MM-DD'
-          )
+          values.drinking_quit_time = values.drinking_quit_time.format('YYYY-MM-DD')
         }
 
         if (values.smoke_quit_time) {
@@ -108,13 +91,7 @@ class PatientHistory extends React.Component {
         const smoke = {}
         const drinking = {}
 
-        for (const type of [
-          'smoke',
-          'smoke_isquit',
-          'smoke_quit_time',
-          'smoke_size',
-          'smoke_year'
-        ]) {
+        for (const type of ['smoke', 'smoke_isquit', 'smoke_quit_time', 'smoke_size', 'smoke_year']) {
           smoke[type] = values[type]
           delete values[type]
         }
@@ -149,19 +126,8 @@ class PatientHistory extends React.Component {
   render() {
     const { getFieldDecorator } = this.props.form
     const { patient_history } = this.props.crf_first_diagnose
-    const submitLoading = this.props.loading.effects[
-      'crf_first_diagnose/modifyPatientHistory'
-    ]
-    const {
-      surgery,
-      tumor_ill,
-      drug_allergy,
-      drug_use,
-      smoke,
-      smoke_isquit,
-      drinking,
-      drinking_is_quit
-    } = this.state
+    const submitLoading = this.props.loading.effects['crf_first_diagnose/modifyPatientHistory']
+    const { surgery, tumor_ill, drug_allergy, drug_use, smoke, smoke_isquit, drinking, drinking_is_quit } = this.state
 
     return (
       <Form {...formItemLayout} onSubmit={this.handleSubmit}>
@@ -174,17 +140,11 @@ class PatientHistory extends React.Component {
               <Radio value="不详">不详</Radio>
               <Radio value="其他">
                 其他
-                {surgery === '其他' ||
-                (surgery === '' && patient_history.surgery === '其他') ? (
+                {surgery === '其他' || (surgery === '' && patient_history.surgery === '其他') ? (
                   <div style={{ display: 'inline-block' }}>
                     {getFieldDecorator('surgery_other', {
                       initialValue: patient_history.surgery_other
-                    })(
-                      <Input
-                        style={{ width: 200, marginLeft: 15 }}
-                        placeholder="其他外伤及手术史"
-                      />
-                    )}
+                    })(<Input style={{ width: 200, marginLeft: 15 }} placeholder="其他外伤及手术史" />)}
                   </div>
                 ) : null}
               </Radio>
@@ -224,8 +184,7 @@ class PatientHistory extends React.Component {
           </Form.Item>
           <Form.Item className={styles.from_item}>
             {getFieldDecorator('base_ill[慢性阻塞性肺疾病]', {
-              initialValue:
-                patient_history['base_ill[慢性阻塞性肺疾病]'] === 'on',
+              initialValue: patient_history['base_ill[慢性阻塞性肺疾病]'] === 'on',
               valuePropName: 'checked'
             })(<Checkbox>慢性阻塞性肺疾病</Checkbox>)}
           </Form.Item>
@@ -261,8 +220,7 @@ class PatientHistory extends React.Component {
           </Form.Item>
           <Form.Item className={styles.from_item}>
             {getFieldDecorator('base_ill[风湿免疫性疾病]', {
-              initialValue:
-                patient_history['base_ill[风湿免疫性疾病]'] === 'on',
+              initialValue: patient_history['base_ill[风湿免疫性疾病]'] === 'on',
               valuePropName: 'checked'
             })(<Checkbox>风湿免疫性疾病</Checkbox>)}
           </Form.Item>
@@ -293,17 +251,11 @@ class PatientHistory extends React.Component {
               <Radio value="不详">不详</Radio>
               <Radio value="其他">
                 其他
-                {tumor_ill === '其他' ||
-                (tumor_ill === '' && patient_history.tumor_ill === '其他') ? (
+                {tumor_ill === '其他' || (tumor_ill === '' && patient_history.tumor_ill === '其他') ? (
                   <div style={{ display: 'inline-block' }}>
                     {getFieldDecorator('tumor_ill_other', {
                       initialValue: patient_history.tumor_ill_other
-                    })(
-                      <Input
-                        style={{ width: 200, marginLeft: 15 }}
-                        placeholder="其他肿瘤病史"
-                      />
-                    )}
+                    })(<Input style={{ width: 200, marginLeft: 15 }} placeholder="其他肿瘤病史" />)}
                   </div>
                 ) : null}
               </Radio>
@@ -314,25 +266,16 @@ class PatientHistory extends React.Component {
           {getFieldDecorator('drug_allergy', {
             initialValue: patient_history.drug_allergy
           })(
-            <Radio.Group
-              onChange={e => this.handleStateChange('drug_allergy', e)}
-            >
+            <Radio.Group onChange={e => this.handleStateChange('drug_allergy', e)}>
               <Radio value="无">无</Radio>
               <Radio value="不详">不详</Radio>
               <Radio value="其他">
                 其他
-                {drug_allergy === '其他' ||
-                (drug_allergy === '' &&
-                  patient_history.drug_allergy === '其他') ? (
+                {drug_allergy === '其他' || (drug_allergy === '' && patient_history.drug_allergy === '其他') ? (
                   <div style={{ display: 'inline-block' }}>
                     {getFieldDecorator('drug_allergy_other', {
                       initialValue: patient_history.drug_allergy_other
-                    })(
-                      <Input
-                        style={{ width: 200, marginLeft: 15 }}
-                        placeholder="其他药物过敏史"
-                      />
-                    )}
+                    })(<Input style={{ width: 200, marginLeft: 15 }} placeholder="其他药物过敏史" />)}
                   </div>
                 ) : null}
               </Radio>
@@ -348,17 +291,11 @@ class PatientHistory extends React.Component {
               <Radio value="不详">不详</Radio>
               <Radio value="其他">
                 其他
-                {drug_use === '其他' ||
-                (drug_use === '' && patient_history.drug_use === '其他') ? (
+                {drug_use === '其他' || (drug_use === '' && patient_history.drug_use === '其他') ? (
                   <div style={{ display: 'inline-block' }}>
                     {getFieldDecorator('drug_use_other', {
                       initialValue: patient_history.drug_use_other
-                    })(
-                      <Input
-                        style={{ width: 200, marginLeft: 15 }}
-                        placeholder="其他药物使用史"
-                      />
-                    )}
+                    })(<Input style={{ width: 200, marginLeft: 15 }} placeholder="其他药物使用史" />)}
                   </div>
                 ) : null}
               </Radio>
@@ -371,11 +308,7 @@ class PatientHistory extends React.Component {
               initialValue: patient_history.smoke === 'on',
               valuePropName: 'checked'
             })(
-              <Switch
-                onChange={e => this.handleStateChange('smoke', e)}
-                checkedChildren="有"
-                unCheckedChildren="无"
-              />
+              <Switch onChange={e => this.handleStateChange('smoke', e)} checkedChildren="有" unCheckedChildren="无" />
             )}
           </Form.Item>
           {smoke || (smoke === null && patient_history.smoke === 'on') ? (
@@ -384,25 +317,22 @@ class PatientHistory extends React.Component {
                 平均吸烟(支/天):{' '}
                 {getFieldDecorator('smoke_size', {
                   initialValue: patient_history.smoke_size
-                })(
-                  <Input
-                    style={{ width: 200, marginLeft: 15 }}
-                    placeholder=""
-                  />
-                )}
+                })(<Input style={{ width: 200, marginLeft: 15 }} placeholder="" />)}
               </Form.Item>
               <Form.Item className={styles.smock_item}>
                 吸烟年数：
                 {getFieldDecorator('smoke_year', {
                   initialValue: patient_history.smoke_year
-                })(
-                  <Input
-                    style={{ width: 200, marginLeft: 15 }}
-                    placeholder=""
-                  />
-                )}
+                })(<Input style={{ width: 200, marginLeft: 15 }} placeholder="" />)}
               </Form.Item>
               <br />
+              <Form.Item className={styles.smock_item}>
+                化疗期间是否吸烟：
+                {getFieldDecorator('smoke_chemotherapy', {
+                  initialValue: patient_history.smoke_chemotherapy === 'on',
+                  valuePropName: 'checked'
+                })(<Switch checkedChildren="是" unCheckedChildren="否" />)}
+              </Form.Item>
               <Form.Item className={styles.smock_item}>
                 是否戒烟：
                 {getFieldDecorator('smoke_isquit', {
@@ -416,26 +346,15 @@ class PatientHistory extends React.Component {
                   />
                 )}
               </Form.Item>
-              {smoke_isquit ||
-              (smoke_isquit === null &&
-                patient_history.smoke_isquit === 'on') ? (
-                <>
-                  <Form.Item className={styles.smock_item}>
-                    戒烟时间：
-                    {getFieldDecorator('smoke_quit_time', {
-                      initialValue: patient_history.smoke_quit_time
-                        ? moment(patient_history.smoke_quit_time, 'YYYY-MM-DD')
-                        : null
-                    })(<DatePicker format="YYYY-MM-DD" />)}
-                  </Form.Item>
-                  <Form.Item className={styles.smock_item}>
-                    化疗期间是否吸烟：
-                    {getFieldDecorator('smoke_chemotherapy', {
-                      initialValue: patient_history.smoke_chemotherapy === 'on',
-                      valuePropName: 'checked'
-                    })(<Switch checkedChildren="是" unCheckedChildren="否" />)}
-                  </Form.Item>
-                </>
+              {smoke_isquit || (smoke_isquit === null && patient_history.smoke_isquit === 'on') ? (
+                <Form.Item className={styles.smock_item}>
+                  戒烟时间：
+                  {getFieldDecorator('smoke_quit_time', {
+                    initialValue: patient_history.smoke_quit_time
+                      ? moment(patient_history.smoke_quit_time, 'YYYY-MM-DD')
+                      : null
+                  })(<DatePicker format="YYYY-MM-DD" />)}
+                </Form.Item>
               ) : null}
             </>
           ) : null}
@@ -453,8 +372,7 @@ class PatientHistory extends React.Component {
               />
             )}
           </Form.Item>
-          {drinking ||
-          (drinking === null && patient_history.drinking === 'on') ? (
+          {drinking || (drinking === null && patient_history.drinking === 'on') ? (
             <>
               <Form.Item className={styles.drinking_item}>
                 <span>饮酒频率：</span>
@@ -484,44 +402,34 @@ class PatientHistory extends React.Component {
               </Form.Item>
               <br />
               <Form.Item className={styles.drinking_item}>
+                化疗期间是否饮酒：
+                {getFieldDecorator('drinking_chemotherapy', {
+                  initialValue: patient_history.drinking_chemotherapy === 'on',
+                  valuePropName: 'checked'
+                })(<Switch checkedChildren="是" unCheckedChildren="否" />)}
+              </Form.Item>
+              <Form.Item className={styles.drinking_item}>
                 是否戒酒：
                 {getFieldDecorator('drinking_is_quit', {
                   initialValue: patient_history.drinking_is_quit === 'on',
                   valuePropName: 'checked'
                 })(
                   <Switch
-                    onChange={e =>
-                      this.handleStateChange('drinking_is_quit', e)
-                    }
+                    onChange={e => this.handleStateChange('drinking_is_quit', e)}
                     checkedChildren="是"
                     unCheckedChildren="否"
                   />
                 )}
               </Form.Item>
-              {drinking_is_quit ||
-              (drinking_is_quit === null &&
-                patient_history.drinking_is_quit === 'on') ? (
-                <>
-                  <Form.Item className={styles.drinking_item}>
-                    戒酒时间：
-                    {getFieldDecorator('drinking_quit_time', {
-                      initialValue: patient_history.drinking_quit_time
-                        ? moment(
-                            patient_history.drinking_quit_time,
-                            'YYYY-MM-DD'
-                          )
-                        : null
-                    })(<DatePicker format="YYYY-MM-DD" />)}
-                  </Form.Item>
-                  <Form.Item className={styles.drinking_item}>
-                    化疗期间是否饮酒：
-                    {getFieldDecorator('drinking_chemotherapy', {
-                      initialValue:
-                        patient_history.drinking_chemotherapy === 'on',
-                      valuePropName: 'checked'
-                    })(<Switch checkedChildren="是" unCheckedChildren="否" />)}
-                  </Form.Item>
-                </>
+              {drinking_is_quit || (drinking_is_quit === null && patient_history.drinking_is_quit === 'on') ? (
+                <Form.Item className={styles.drinking_item}>
+                  戒酒时间：
+                  {getFieldDecorator('drinking_quit_time', {
+                    initialValue: patient_history.drinking_quit_time
+                      ? moment(patient_history.drinking_quit_time, 'YYYY-MM-DD')
+                      : null
+                  })(<DatePicker format="YYYY-MM-DD" />)}
+                </Form.Item>
               ) : null}
             </>
           ) : null}
