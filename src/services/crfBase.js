381c706fdff7c5b28faadbb509d316230247f484
IcebearDL <u201616984@hust.edu.cn>
2020-04-14 17:15:48 +0800

    feat: Add sign api.

--- src/services/crfBase.js
+++ src/services/crfBase.js
@@ -28,10 +28,61 @@ export async function DeleteCycle({ sample_id }) {
   })
 }
 
+// 提交cycle记录
+export async function PostCycle({ sample_id, cycle_number }) {
+  return request(`/submit_cycle/${sample_id}/${cycle_number}`, {
+    method: 'POST'
+  })
+}
+
+// 获取基线资料的签名
+export async function FetchBaseSignature({ sample_id }) {
+  return request(`/sample/signature/${sample_id}`, {
+    method: 'GET'
+  })
+}
+
+// 添加基线资料的签名
+export async function PostBaseSignature({ sample_id, user_id }) {
+  return request(`/sample/signature/${sample_id}`, {
+    method: 'POST',
+    data: { user_id }
+  })
+}
+
+// 获取项目总结的签名
+export async function FetchSummarySignature({ sample_id }) {
+  return request(`/summary/signature/${sample_id}`, {
+    method: 'GET'
+  })
+}
+
+// 添加项目总结的签名
+export async function PostSummarySignature({ sample_id, user_id }) {
+  return request(`/summary/signature/${sample_id}`, {
+    method: 'POST',
+    data: { user_id }
+  })
+}
+
+// 获取随访提交状态信息
+export async function FetchCycleStatus({ sample_id }) {
+  return request(`/cycle/submit_info/${sample_id}`, {
+    method: 'GET'
+  })
+}
+
 // 获取生存期随访信息
 export async function FetchInterviewTable({ sample_id }) {
   return request(`/interview_table/${sample_id}`, {
     method: 'GET'
+  })
+}
+
+// 提交单条生存期随访
+export async function PostInterview({ interview_id }) {
+  return request(`/interview/submit/${interview_id}`, {
+    method: 'POST'
   })
 }
 
@@ -68,6 +119,13 @@ export async function ModifySummary({ sample_id, body }) {
 // 获取全部严重不良事件
 export function FetchAdverseEventAll({ sample_id }) {
   return request(`/adverse_event_table_all/${sample_id}`, {
+    method: 'GET'
+  })
+}
+
+// 获取影像学评估的文件列表
+export function FetchPhotoFileList({ sample_id, cycle_number, evaluate_id }) {
+  return request(`/photo_evaluate/file/${sample_id}/${cycle_number}/${evaluate_id}`, {
     method: 'GET'
   })
 }
