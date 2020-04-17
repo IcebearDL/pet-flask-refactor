438758793dbde39e7afc7a90450a7b1e2e5e655c
IcebearDL <u201616984@hust.edu.cn>
2020-04-17 11:39:22 +0800

    feat(Add codeMessage and ENV.):

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,24 +6,34 @@ import CookieUtil from './cookie'
 import { removeNull } from './util'
 
 const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。'
+  999: '服务器出错',
+  1000: '用户无效',
+  1001: '无效参数',
+  1002: '资源未找到',
+  1003: '用户授权失败',
+  1004: '文件查找失败',
+  1005: '上传文件失败',
+  1006: '样本非该用户所创建',
+  1007: '样本已提交,无法修改',
+  10021: '该资源已删除',
+  10031: 'token无效',
+  10032: 'token过期',
+  10033: '该用户名不存在',
+  10034: '用户名或密码错误'
 }
 
-// prefix 阿里云的测试服务器
+const config = {
+  // 预生产预览环境  阿里云的测试服务器
+  pre: 'http://39.96.191.139:8080',
+  // 生产环境地址
+  prod: 'http://rayplus.top'
+}
+
 const { NODE_ENV } = process.env
-const prefix = NODE_ENV === 'development' ? '/api' : 'http://39.96.191.139'
+const ENV = 'pre'
+
+export const post_prefix = config[ENV]
+const prefix = NODE_ENV === 'development' ? '/api' : config[ENV]
 
 let COOKIE_CONFIRM = true
 const auth_request = (url, { method = 'GET', params = {}, data = {} }) => {
@@ -43,7 +53,7 @@ const auth_request = (url, { method = 'GET', params = {}, data = {} }) => {
   return new Promise(resolve => {
     request(prefix + url, {
       method,
-      params,
+      params: removeNull(params),
       data: removeNull(data),
       // 这里的request的header不能加在extend创建实例里
       headers: {
@@ -53,7 +63,11 @@ const auth_request = (url, { method = 'GET', params = {}, data = {} }) => {
     }).then(res => {
       if (res && res.code === 200) {
         // 如果post请求没有data，就返回true，以便判断generator下一步执行
-        resolve(res.data || true)
+        if (res.total) {
+          resolve({ data: res.data, total: res.total })
+        } else {
+          resolve(res.data || true)
+        }
       } else {
         notification.error({
           message: codeMessage[res.code || 406],
