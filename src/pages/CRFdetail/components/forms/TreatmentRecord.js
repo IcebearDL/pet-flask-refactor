75b3f83ca06bfa97b5b7397bda061b39bc13c61c
IcebearDL <u201616984@hust.edu.cn>
2020-04-22 18:11:42 +0800

    fix(TreatmentRecord.js): 拆分治疗记录单的表单。解决ant form内只能有一个提交。

--- src/pages/CRFdetail/components/forms/TreatmentRecord.js
+++ src/pages/CRFdetail/components/forms/TreatmentRecord.js
@@ -6,14 +6,118 @@ import moment from 'moment'
 import { getSampleId } from '@/utils/location'
 import styles from '../../style.css'
 
+// 治疗记录单下方的表格
+class Treatment extends React.Component {
+  state = {
+    adjustment: ''
+  }
+
+  static propTypes = {
+    crf_cycle_record: PropTypes.object.isRequired,
+    cycle_number: PropTypes.number.isRequired,
+    form: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired,
+    loading: PropTypes.object.isRequired
+  }
+
+  handleSubmitAdjustment = e => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        const { dispatch, cycle_number } = this.props
+        const sample_id = getSampleId()
+        const adjustment_status = {}
+
+        if (values.adjustment === '0') {
+          adjustment_status.adjustment = '0'
+        } else if (values.adjustment === '1') {
+          adjustment_status.adjustment = '1'
+          for (const type of ['adjust_percent', 'adjust_reason']) {
+            if (values[type]) {
+              adjustment_status[type] = values[type]
+            }
+          }
+        }
+
+        dispatch({
+          type: 'crf_cycle_record/modifyTreatmentStatusRecord',
+          payload: { sample_id, cycle_number, body: { adjustment_status } }
+        }).then(() => {
+          dispatch({
+            type: 'crf_cycle_record/fetchTreatmentStatusRecord',
+            payload: { sample_id, cycle_number }
+          })
+        })
+      }
+    })
+  }
+
+  handleStateChange = ({ target: { value } }) => {
+    this.setState({ adjustment: value })
+  }
+
+  render() {
+    const { treatment_record_adjustment_status } = this.props.crf_cycle_record
+    const submitLoadingAdjustment = this.props.loading.effects['crf_cycle_record/modifyTreatmentStatusRecord']
+    const { getFieldDecorator } = this.props.form
+    const { adjustment } = this.state
+
+    return (
+      <Form
+        className="page_body"
+        style={{ marginTop: '20px' }}
+        labelAlign="right"
+        labelCol={{ span: 6 }}
+        wrapperCol={{ span: 17, offset: 1 }}
+        onSubmit={this.handleSubmitAdjustment}
+      >
+        <Form.Item label="治疗中用药剂量有无调整">
+          {getFieldDecorator('adjustment', {
+            rules: [{ required: true, message: '请选择剂量调整情况' }],
+            initialValue: treatment_record_adjustment_status.adjustment
+          })(
+            <Radio.Group onChange={this.handleStateChange}>
+              <Radio value="0">无</Radio>
+              <Radio value="1">有</Radio>
+            </Radio.Group>
+          )}
+        </Form.Item>
+        {adjustment === '1' || (adjustment === '' && treatment_record_adjustment_status.adjustment === '1') ? (
+          <>
+            <Form.Item label="调整为标准剂量的百分比">
+              {getFieldDecorator('adjust_percent', {
+                initialValue: treatment_record_adjustment_status.adjust_percent
+              })(<Input style={{ width: '250px' }} placeholder="请输入百分比数值(%)" />)}
+            </Form.Item>
+            <Form.Item label="调整原因">
+              {getFieldDecorator('adjust_reason', {
+                initialValue: treatment_record_adjustment_status.adjust_reason
+              })(<Input style={{ width: '250px' }} placeholder="请输入调整原因" />)}
+            </Form.Item>
+          </>
+        ) : null}
+        <Col offset={7}>
+          <Button htmlType="submit" type="primary" loading={submitLoadingAdjustment}>
+            保存
+          </Button>
+        </Col>
+      </Form>
+    )
+  }
+}
+
+const TreatmentForm = connect(state => {
+  return {
+    crf_cycle_record: state.crf_cycle_record,
+    loading: state.loading
+  }
+})(Form.create()(Treatment))
+
+// 治疗记录单
 class TreatmentRecord extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      record: {},
-      visible: false,
-      adjustment: ''
-    }
+  state = {
+    record: {},
+    visible: false
   }
 
   static propTypes = {
@@ -60,22 +164,17 @@ class TreatmentRecord extends React.Component {
         // 重构时间和其他空项
         if (values.start_time) {
           adjustment.start_time = values.start_time.format('YYYY-MM-DD')
-        } else {
-          adjustment.start_time = ''
         }
         if (values.end_time) {
           adjustment.end_time = values.end_time.format('YYYY-MM-DD')
-        } else {
-          adjustment.end_time = ''
         }
         for (const type of ['description', 'medicine_name', 'treatment_name']) {
-          if (!values[type]) {
-            adjustment[type] = ''
-          } else {
+          if (values[type]) {
             adjustment[type] = values[type]
           }
         }
         adjustment.treatment_record_id = record.treatment_record_id
+
         dispatch({
           type: 'crf_cycle_record/modifyTreatmentRecord',
           payload: { sample_id, cycle_number, body: adjustment }
@@ -90,54 +189,12 @@ class TreatmentRecord extends React.Component {
     })
   }
 
-  handleSubmitAdjustment = e => {
-    e.preventDefault()
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        const { dispatch, cycle_number } = this.props
-        const sample_id = getSampleId()
-        const adjustment_status = {}
-
-        if (values.adjustment === '0') {
-          adjustment_status.adjustment = '0'
-          adjustment_status.adjust_percent = ''
-          adjustment_status.adjust_reason = ''
-        } else if (values.adjustment === '1') {
-          adjustment_status.adjustment = '1'
-          for (const type of ['adjust_percent', 'adjust_reason']) {
-            if (!values[type]) {
-              adjustment_status[type] = ''
-            } else {
-              adjustment_status[type] = values[type]
-            }
-          }
-        } else if (!values.adjustment) {
-          adjustment_status.adjust_percent = ''
-          adjustment_status.adjust_reason = ''
-        }
-        dispatch({
-          type: 'crf_cycle_record/modifyTreatmentStatusRecord',
-          payload: { sample_id, cycle_number, body: { adjustment_status } }
-        }).then(() => {
-          dispatch({
-            type: 'crf_cycle_record/fetchTreatmentStatusRecord',
-            payload: { sample_id, cycle_number }
-          })
-        })
-      }
-    })
-  }
-
   handleEditModel = record => {
     this.setState({ record, visible: true })
   }
 
   handleCancel = () => {
     this.setState({ visible: false })
-  }
-
-  handleStateChange = ({ target: { value } }) => {
-    this.setState({ adjustment: value })
   }
 
   columns = [
@@ -188,12 +245,11 @@ class TreatmentRecord extends React.Component {
   ]
 
   render() {
-    const { treatment_record_table, treatment_record_adjustment_status } = this.props.crf_cycle_record
+    const { treatment_record_table } = this.props.crf_cycle_record
     const tableLoading = this.props.loading.effects['crf_cycle_record/fetchTreatmentRecord']
     const submitLoading = this.props.loading.effects['crf_cycle_record/modifyTreatmentRecord']
-    const submitLoadingAdjustment = this.props.loading.effects['crf_cycle_record/modifyTreatmentStatusRecord']
     const { getFieldDecorator } = this.props.form
-    const { record, visible, adjustment } = this.state
+    const { record, visible } = this.state
 
     return (
       <>
@@ -211,45 +267,7 @@ class TreatmentRecord extends React.Component {
           columns={this.columns}
           dataSource={treatment_record_table}
         />
-        <Form
-          className="page_body"
-          style={{ marginTop: '20px' }}
-          labelAlign="right"
-          labelCol={{ span: 6 }}
-          wrapperCol={{ span: 17, offset: 1 }}
-          onSubmit={this.handleSubmitAdjustment}
-        >
-          <Form.Item label="治疗中用药剂量有无调整">
-            {getFieldDecorator('adjustment', {
-              rules: [{ required: true, message: '请选择剂量调整情况' }],
-              initialValue: treatment_record_adjustment_status.adjustment
-            })(
-              <Radio.Group onChange={this.handleStateChange}>
-                <Radio value="0">无</Radio>
-                <Radio value="1">有</Radio>
-              </Radio.Group>
-            )}
-          </Form.Item>
-          {adjustment === '1' || (adjustment === '' && treatment_record_adjustment_status.adjustment === '1') ? (
-            <>
-              <Form.Item label="调整为标准剂量的百分比">
-                {getFieldDecorator('adjust_percent', {
-                  initialValue: treatment_record_adjustment_status.adjust_percent
-                })(<Input style={{ width: '250px' }} placeholder="请输入百分比数值(%)" />)}
-              </Form.Item>
-              <Form.Item label="调整原因">
-                {getFieldDecorator('adjust_reason', {
-                  initialValue: treatment_record_adjustment_status.adjust_reason
-                })(<Input style={{ width: '250px' }} placeholder="请输入调整原因" />)}
-              </Form.Item>
-            </>
-          ) : null}
-          <Col offset={7}>
-            <Button htmlType="submit" type="primary" loading={submitLoadingAdjustment}>
-              保存
-            </Button>
-          </Col>
-        </Form>
+        <TreatmentForm cycle_number={this.props.cycle_number} />
         <Modal
           title="编辑治疗记录单"
           visible={visible}
